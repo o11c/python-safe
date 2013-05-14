0283460e332892d1be79e27c5884b2ec49b2d219
Ben Longbons <b.r.longbons@gmail.com>
2013-05-14 16:49:03 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(
+    name='safe',
+    version='1.0',
+    description='Type-safety utilities for python3',
+    author='Ben Longbons',
+    author_email='b.r.longbons@gmail.com',
+    url='http://github.com/o11c/python-safe',
+    packages=[
+        'safe'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+    ]
+)
